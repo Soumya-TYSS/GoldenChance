cfbdd159acb721e825fff724cfa05df128333339
naveen magnur <naveen magnur@magnur>
2022-07-18 12:42:08 +0530

    Added by sanjay

--- src/test/java/practice/SelectQueryTest.java
+++ src/test/java/practice/SelectQueryTest.java
@@ -19,6 +19,7 @@ public class SelectQueryTest {
 		System.out.println("connection is done");
 		
 		System.out.println("This statment is added by sanjay");
+		System.out.println("Another statment added by sanjay");
 		//create query statemnet
 		Statement stat = conn.createStatement();
 		//execute the query
